9651fc8e81782c4da6b4bb6797a5f0faa8a364e2
Sushanata Gupta <sushantagupta007.gmail.com>
2021-08-11 02:16:29 +0600

    solved code

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-let newArray = [1,2,3,4,5,6,7] ;
-for (const item of newArray){
-    if(item<3){
-        
-        continue;
-    }
-    console.log(item);
-}
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */    
+    let re = /^([aeiou]).+\1$/
+    
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;

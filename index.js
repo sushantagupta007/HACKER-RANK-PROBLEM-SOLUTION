38b532431485c44831b6abc097aad4170215a347
Sushanata Gupta <sushantagupta007.gmail.com>
2021-08-11 22:39:32 +0600

    code added

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-function reverseString(s) {
-    
-    if(typeof s ==='number'){
-        console.log("s.split is not a function");
-        console.log(s);
+function isPositive(a) {
+    if(a>0){
+        return "YES"; 
     }
-    if(typeof s ==='string'){
-        const splitStrin = s.split("");
-        const reverseStrin = splitStrin.reverse();
-        const reverse = reverseStrin.join("");
-        console.log(reverse);
-    } 
+    if(a===0){
+        return "Zero Error" ;
+    }
+    if(a<0){
+        return "Negative Error";
+    }
 }
-
-//Note: To Reverse a string first split it and form a array. string.split(""); ** It indicates no gap
-// Then reverse it reverse();
-// Then join it .join("") **It indicates No Gap 
